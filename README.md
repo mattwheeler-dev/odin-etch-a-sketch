d32c8130feaa24cf8a74f2b350051398ddca921f
Matthew <105622101+mattwheeler-dev@users.noreply.github.com>
2026-03-05 15:23:50 -0800

    full v2 revamp;
    implement new mode features and range selector for new grids instead of pop up input. complete restyle as well.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# odin-etch-a-sketch
-Odin Project Etch-a-Sketch
+# Etch-a-Sketch
+
+## New & Improved
+
+### Features:
+
+<ul>
+<li>Color picker to select the color you draw with when using solid mode</li>
+<li>Rainbow mode which selects a randomized color for every box when drawing</li>
+<li>Eraser mode, allowing you to remove the color from boxes</li>
+<li>A clear grid button which quickly erases all boxes</li>
+<li>Use the range input to select a new grid size, and the new grid will be created when you press the new grid button</li>
+</ul>
--- scripts.js
+++ scripts.js
@@ -1,46 +1,64 @@
-const container = document.querySelector(".container");
-const newGrid = document.querySelector("#new-grid");
+const colorPicker = document.querySelector('#color-picker');
+const solidBtn = document.querySelector('#solid-btn');
+const rainbowBtn = document.querySelector('#rainbow-btn');
+const eraserBtn = document.querySelector('#eraser-btn');
+const modeBtns = [solidBtn, rainbowBtn, eraserBtn];
+const clearBtn = document.querySelector('#clear-btn');
+const gridSize = document.querySelector('#grid-size');
+const newGridBtn = document.querySelector('#new-grid-btn');
+const gridRange = document.querySelector('#grid-range');
+const grid = document.querySelector('#grid');
 
-// CREATE 16 * 16 GRID (256)
-for (i = 0; i < 256; i++) {
-	const box = document.createElement("div");
-
-	box.classList.add("box");
-
-	// HOVER EFFECT FOR BOXES
-	box.addEventListener("mouseover", () => {
-		box.style.backgroundColor = "#fff";
-	});
-
-	container.appendChild(box);
-}
-
-// NEW GRID BTN & PROMPT
-newGrid.addEventListener("click", () => {
-	let gridSize = prompt(
-		"Please enter the number of squares you would like per side for your new grid. (Max: 64)",
-		"10"
-	);
-
-	// USE INPUT TO CREATE NEW GRID
-	if (gridSize == null || gridSize < 0 || gridSize > 64) {
-		alert(
-			"Please try again with a number between (but not including) 0 and 65"
-		);
-	} else if (gridSize != null && 0 < Number(gridSize) < 65) {
-		container.replaceChildren();
-
-		// SQUARE INPUT FOR GRID & APPLY SIZING
-		for (let i = 0; i < gridSize * gridSize; i++) {
-			const box = document.createElement("div");
-			box.style.height = `${Math.floor(600 / gridSize)}px`;
-			box.style.width = `${Math.floor(600 / gridSize)}px`;
-			container.appendChild(box);
-
-			// HOVER EFFECT FOR NEW BOXES
-			box.addEventListener("mouseover", () => {
-				box.style.backgroundColor = "#fff";
-			});
-		}
+const newGrid = (size = 16) => {
+	grid.replaceChildren();
+	for (let i = 0; i < size * size; i++) {
+		const box = document.createElement('div');
+		box.classList.add('box');
+		box.style.width = `${100 / size}%`;
+		box.style.height = `${100 / size}%`;
+		grid.appendChild(box);
+		box.addEventListener('mouseover', (e) => {
+			if (solidBtn.classList.contains('active')) {
+				e.target.style.backgroundColor = colorPicker.value;
+			} else if (rainbowBtn.classList.contains('active')) {
+				e.target.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+			} else if (eraserBtn.classList.contains('active')) {
+				e.target.style.backgroundColor = 'transparent';
+			}
+		});
 	}
+	gridSize.textContent = `${size} x ${size}`;
+};
+
+const setMode = (mode = 'solid') => {
+	modeBtns.forEach((btn) => {
+		btn.addEventListener('click', () => {
+			setMode(btn.id.split('-')[0]);
+		});
+		if (btn.id === `${mode}-btn`) {
+			btn.classList.add('active');
+		} else {
+			btn.classList.remove('active');
+		}
+	});
+};
+
+const clearGrid = () => {
+	const boxes = document.querySelectorAll('.box');
+	boxes.forEach((box) => {
+		box.style.backgroundColor = 'transparent';
+	});
+};
+
+clearBtn.addEventListener('click', clearGrid);
+gridRange.addEventListener('input', () => {
+	gridSize.textContent = `${gridRange.value} x ${gridRange.value}`;
 });
+newGridBtn.addEventListener('click', () => newGrid(gridRange.value));
+
+setMode();
+newGrid();
+
+const footer = document.querySelector('footer p');
+const year = new Date().getFullYear();
+footer.textContent = `© ${year} mattwheeler-dev`;
